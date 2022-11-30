7da1960545fe75d6988e4c9e55f7ab95163f42d6
Talal Alenezi <98838632+Talal-Alenezi@users.noreply.github.com>
2022-11-30 12:44:17 +0300

    15 words

--- My project/Assets/TranslationController.cs
+++ My project/Assets/TranslationController.cs
@@ -48,6 +48,61 @@ public class TranslationController : MonoBehaviour
         {
             anim.SetTrigger("thx");
         }
+        else if (testresponse == "engineer")
+        {
+            anim.SetTrigger("engineer");
+        }
+        else if (testresponse == "student")
+        {
+            anim.SetTrigger("student");
+        }
+        else if (testresponse == "there is an exam next lecture" || testresponse == "exam next lecture" || testresponse == "there is an exam next class" || testresponse == "exam next class")
+        {
+            anim.SetTrigger("exam next class");
+        }
+        else if (testresponse == "10 to the power of 0 equal to 1" || testresponse == "10^0=1" || testresponse == "10^0 equal 1" || testresponse == "10^0 equal one" || testresponse == "10 power 0 equal 1")
+        {
+            anim.SetTrigger("10 to power of 0");
+        } 
+        else if (testresponse == "book")
+        {
+            anim.SetTrigger("book");
+        }
+        else if (testresponse == "calculus")
+        {
+            anim.SetTrigger("calculus");
+        }
+        else if (testresponse == "focus")
+        {
+            anim.SetTrigger("focus");
+        }
+        else if (testresponse == "find the value of x" || testresponse == "find value of x" || testresponse == "find the value of X" || testresponse == "find value of X")
+        {
+            anim.SetTrigger("find the value of x");
+        }
+        else if (testresponse == "i'm hungry")
+        {
+            anim.SetTrigger("im hungry");
+        }
+        else if (testresponse == "projector")
+        {
+            anim.SetTrigger("projector");
+        }
+        else if (testresponse == "raise your hand")
+        {
+            anim.SetTrigger("raise your hand");
+        }
+        else if (testresponse == "solve the problem")
+        {
+            anim.SetTrigger("solve the problem");
+        }
+
+
+        //test
+        else if (testresponse == "test")
+        {
+            anim.SetTrigger("test");
+        }
     }
 
     private void InProcessListeningHandler(string testresponse)
